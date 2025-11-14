d22abde927741a4c9e0b72326e0e993c3b6e1715
lucasmz7 <lucasmierez007@gmail.com>
2025-11-14 14:34:15 -0300

    Implementacion materia.c

--- include/materia.h
+++ include/materia.h
@@ -10,9 +10,8 @@ typedef enum {
 } EstadoMateria;
 
 typedef struct {
-    char nombre[40];
+    char nombre[50];
     char identificador[5];
-    int id;
     EstadoMateria estado;
     float nota;
     //struct Materia *materias_correlativas[10]; 
@@ -20,7 +19,7 @@ typedef struct {
 
 //void materia_modificar_correlativas(void);
 void materia_modificar_nombre(Materia *materia, char* nuevo_nombre);
-void materia_modificar_id(Materia *materia, int nuevo_id);
+//void materia_modificar_id(Materia *materia, int nuevo_id);
 void materia_modificar_identificador(Materia *materia, const char* nuevo_identificador);
 
 #endif
--- materia.c
+++ materia.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "materia.h"
+#include <utils.c>
 
-typedef struct Materia{
-    char nombre[40];
-    char identificador[5];
-    bool rendida;
-    float nota;
-    Materia materias_correlativas[10];
-} Materia;
+// Modificar los datos de una materia del listado global
+void materia_modificar_nombre(Materia *materia, const char* nuevo_nombre) {
+    if (verificar_nombre_materia(nuevo_nombre) != 0) {
+        printf("No se pudo modificar el nombre de la materia. Nombre invalido");
+        return;
+    }
+}
+
+void materia_modificar_identificador(Materia *materia, const char* nuevo_identificador) {
+    if (verificar_identificador(nuevo_identificador) != 0) {
+        printf("No se pudo modificar el identificador de la materia. Identificador invalido");
+        return;        
+    }
+}
+
+//void materia_modificar_id(Materia *materia, int nuevo_id) {}
+
