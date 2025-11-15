de808789dc2165bdf1b3a705157fd7f3f0d17c5f
lucasmz7 <lucasmierez007@gmail.com>
2025-11-14 22:29:05 -0300

    Fix

--- include/estudiante.h
+++ include/estudiante.h
@@ -17,7 +17,7 @@ int estudiante_modificar_legajo(Estudiante *estudiante, int nuevo_legajo);
 int estudiante_modificar_edad(Estudiante *estudiante, int nueva_edad); 
 int estudiante_modificar_nombre(Estudiante *estudiante, const char* nuevo_nombre);
 int estudiante_actualizar_promedio(Estudiante *estudiante);
-int rendir_final(Estudiante *estudiante, const char *nombre_materia, int nota);
+int rendir_final(Estudiante *estudiante, const char *nombre_materia, float nota);
 int anotar(Estudiante *estudiante, MateriaGlobal *materia) ;
 int bajar(Estudiante *estudiante, const char* nombre_materia);
 
